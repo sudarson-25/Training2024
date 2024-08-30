bffff20488c75fea676832c442b097d3d97d723a
Sudarson <sudarson.selvakumar@trumpf.com>
2024-08-30 16:12:53 +0530

    A3: Resolved

--- Training/Training/main.c
+++ Training/Training/main.c
@@ -1,3 +1,11 @@
+// ------------------------------------------------------------------------------------------------
+// Training ~ A training program for new joiners at Metamation, Batch - July 2024.
+// Copyright (c) Metamation India.
+// ------------------------------------------------------------------
+// main.c
+// Program to convert and display decimal value into binary and hexadecimal value.
+// ------------------------------------------------------------------------------------------------
+
 #include<stdio.h>
 #include<malloc.h>
 #include<math.h>
@@ -5,10 +13,8 @@
 int mCount;
 
 //Function for counting the number of digits
-int count (int dec, int div) {
+int Count (int dec, int div) {
    int c = 0;
-   if (dec == 0)
-      return 8;
    while (dec > 0) {
       dec /= div;
       c++;
@@ -18,25 +24,23 @@ int count (int dec, int div) {
 
 int* DecToBin (int dec) {
    mCount = 0;
-   int dec2 = dec;
-   if (dec < 0)
-      dec *= -1;
-   int dec1 = dec;
+   int decReal = dec;
+   if (dec < 0) dec *= -1;
+   int decTemp = dec;
    //Counting the number of digits in binary representation
-   mCount = count (dec, 2);
+   mCount = Count (dec, 2);
    // Representing binary in multiples of 8-bits accordingly
-   if (mCount % 8 != 0)
-      mCount += (8 - (mCount % 8));
+   if (mCount % 8 != 0) mCount += (8 - (mCount % 8));
    int* binRev = NULL;
    do {
       binRev = (int*)malloc (mCount * sizeof (int));
    } while (binRev == NULL);
    //Storing the remainders into binRev
    for (int i = 0; i < mCount; i++) {
-      binRev[i] = dec1 % 2;
-      dec1 /= 2;
+      binRev[i] = decTemp % 2;
+      decTemp /= 2;
    }
-   if (dec2 >= 0) {
+   if (decReal > 0) {
       int* bin;
       do {
          bin = (int*)malloc (mCount * sizeof (int));
@@ -52,10 +56,7 @@ int* DecToBin (int dec) {
       } while (binTwosComp == NULL);
       //Complimenting each element of the array
       for (int k = 0; k < mCount; k++) {
-         if (binRev[mCount - k - 1] == 0)
-            binTwosComp[k] = 1;
-         else
-            binTwosComp[k] = 0;
+         binTwosComp[k] = binRev[mCount - k - 1] == 0 ? 1 : 0;
       }
       free (binRev);
       //Adding 1 to the the binary number (stored as individual digits)
@@ -63,8 +64,7 @@ int* DecToBin (int dec) {
          if (binTwosComp[k] == 0) {
             binTwosComp[k] = 1;
             break;
-         } else
-            binTwosComp[k] = 0;
+         } else binTwosComp[k] = 0;
       }
       return binTwosComp;
    }
@@ -72,28 +72,23 @@ int* DecToBin (int dec) {
 
 char* DecToHex (int dec) {
    mCount = 0;
-   int dec2 = dec;
-   if (dec < 0)
-      dec *= -1;
-   int dec1 = dec;
+   int decReal = dec;
+   if (dec < 0) dec *= -1;
+   int decTemp = dec;
    //Counting the number of digits in hex representation
-   mCount = count (dec, 16);
+   mCount = Count (dec, 16);
    // Representing hex in multiples of 8-bits accordingly
-   if (mCount % 8 != 0)
-      mCount += (8 - (mCount % 8));
+   if (mCount % 8 != 0) mCount += (8 - (mCount % 8));
    char* hexRev = NULL;
    do {
       hexRev = (char*)malloc (mCount * sizeof (char));
    } while (hexRev == NULL);
    //Storing the remainders into hexRev
    for (int i = 0; i < mCount; i++) {
-      if (dec1 % 16 >= 10)
-         hexRev[i] = (dec1 % 16) + 55;
-      else
-         hexRev[i] = (dec1 % 16) + 48;
-      dec1 /= 16;
+      hexRev[i] = decTemp % 16 >= 10 ? (decTemp % 16) + 55 : (decTemp % 16) + 48;
+      decTemp /= 16;
    }
-   if (dec2 >= 0) {
+   if (decReal > 0) {
       char* hex = NULL;
       do {
          hex = (char*)malloc (mCount + 1 * sizeof (char));
@@ -107,11 +102,10 @@ char* DecToHex (int dec) {
       return hex;
    } else {
       //Decimal to binary
-      int* bin = DecToBin (dec2);
+      int* bin = DecToBin (decReal);
       int digit = 0, place = 0, icount = 0, count = mCount / 4;
       // Representing hex in multiples of 8-bits accordingly
-      if (count % 8 != 0)
-         count += (8 - (count % 8));
+      if (count % 8 != 0) count += (8 - (count % 8));
       char* hexNegRev = NULL;
       do {
          hexNegRev = (char*)malloc (count * sizeof (char));
@@ -123,12 +117,8 @@ char* DecToHex (int dec) {
          place++;
          icount++;
          if (icount == 4) {
-            place = 0;
-            icount = 0;
-            if (digit >= 10)
-               hexNegRev[ind] = digit + 55;
-            else
-               hexNegRev[ind] = digit + 48;
+            place = 0, icount = 0;
+            hexNegRev[ind] = digit >= 10 ? digit + 55 : digit + 48;
             ind++;
             digit = 0;
          }
@@ -160,25 +150,40 @@ int main () {
       printf ("Input (Enter an integer): ");
       if (scanf_s ("%d%c", &dec, &term, 1) != 2 || term != '\n') {
          printf ("Enter a valid input!");
-         //Draining the inputs from stdin
+         //Draining the unassigned inputs from stdin
          for (;;) {
             term = fgetc (stdin);
-            if (term == '\n')
-               break;
+            if (term == '\n') break;
          }
          printf ("\n\n");
       } else {
-         int* bin = DecToBin (dec);
-         printf ("Binary (%d-bit): ", mCount);
-         for (int i = 0; i < mCount; i++)
-            printf ("%d", bin[i]);
-         char* hex = DecToHex (dec);
-         printf ("\nHEX (%d-bit): ", mCount);
-         for (int i = 0; i < mCount; i++)
-            printf ("%c", hex[i]);
-         free (bin);
-         free (hex);
-         printf ("\n\n");
+         if (dec == 0) {
+            int converted[8];
+            for (int i = 0; i < 8; i++) {
+               converted[i] = 0;
+            }
+            printf ("Binary (%d-bit): ", 8);
+            for (int i = 0; i < 8; i++) {
+               printf ("%d", converted[i]);
+            }
+            printf ("\nHEX (%d-bit): ", 8);
+            for (int i = 0; i < 8; i++) {
+               printf ("%d", converted[i]);
+            }
+            printf ("\n\n");
+         } else {
+            int* bin = DecToBin (dec);
+            printf ("Binary (%d-bit): ", mCount);
+            for (int i = 0; i < mCount; i++)
+               printf ("%d", bin[i]);
+            char* hex = DecToHex (dec);
+            printf ("\nHEX (%d-bit): ", mCount);
+            for (int i = 0; i < mCount; i++)
+               printf ("%c", hex[i]);
+            free (bin);
+            free (hex);
+            printf ("\n\n");
+         }
       }
    }
    return 0;
