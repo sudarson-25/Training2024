faa17aab1987203656068de8ff74a7f2c354f6af
Sudarson <sudarson.selvakumar@trumpf.com>
2024-11-22 12:40:46 +0530

    Test2: Initial Commit

--- Training/Training/Training.c
+++ Training/Training/Training.c
@@ -3,12 +3,24 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.c
-// Program on main branch.
+// Program to find the number of coins of each denomination required.
 // ------------------------------------------------------------------------------------------------
 
 #include<stdio.h>
 
 int main () {
-	printf ("Hello World!");
-	return 0;
+   int paid, actual, rem, change;
+   printf ("Cash paid: ");
+   scanf_s ("%d", &paid);
+   printf ("Actual Amount: ");
+   scanf_s ("%d", &actual);
+   change = paid - actual;
+   printf ("No. of Rs.10 coins: %d\n", change / 10);
+   rem = change % 10;
+   printf ("No. of Rs.5 coins: %d\n", rem / 5);
+   rem %= 5;
+   printf ("No. of Rs.2 coins: %d\n", rem / 2);
+   rem %= 2;
+   printf ("No. of Re.1 coins: %d\n", rem / 1);
+   return 0;
 }
