a6b9db1215e97a5b5d4adcc2f6eac9db40f99dcd
Sudarson <sudarson.selvakumar@trumpf.com>
2024-11-28 23:19:58 +0530

    Test2.2: Modified array initialization

--- Training/Training/change.c
+++ Training/Training/change.c
@@ -3,7 +3,8 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // change.c
-// Program to perform and test arithmetic operations on complex numbers.
+// Program to calculate the optimal change in coins for cash transactions.
+// Sudarson S
 // ------------------------------------------------------------------------------------------------
 
 #include<stdio.h>
@@ -12,6 +13,7 @@
 #include<stdlib.h>
 #include<string.h> // USED
 
+/// <summary>Calculates optimal change and returns an array of coins of each denomination</summary>
 void OptimalChangeCalculator (int* coins, int rem, int* denominations) {
    for (int i = 0; i < 4; i++) {
       coins[i] = rem / denominations[i];
@@ -19,25 +21,23 @@ void OptimalChangeCalculator (int* coins, int rem, int* denominations) {
    }
 }
 
+/// <summary>Runs test cases</summary>
 static void Testcases (int* denominations) {
-   int input[] = { 95,75,67,79 }, expected[][4] = { 9,1,0,0,7,1,0,0,6,1,1,0,7,1,2,0 }, output[4];
+   int input[] = { 95,75,67,79 }, expected[][4] = { {9,1,0,0},{7,1,0,0},{6,1,1,0},{7,1,2,0} }, output[4],
+      inputs = sizeof (input) / sizeof (input[0]);
    printf ("\nTestcases\n~~~~~~~~~\n");
-   for (int i = 0; i < sizeof (input) / sizeof (input[0]); i++) {
+   for (int i = 0; i < inputs; i++) {
       OptimalChangeCalculator (output, input[i], denominations);
       int j;
-      for (j = 0; j < 4; j++)
-         if (expected[i][j] != output[j]) {
-            printf ("Case %d: FAILED\n", i + 1);
-            break;
-         }
-      if (j == 4) printf ("Case %d: PASSED\n", i + 1);
+      for (j = 0; j < inputs && expected[i][j] == output[j]; j++);
+      printf ("Case %d: %s\n", i + 1, j == inputs ? "PASSED" : "FAILED");
    }
 }
 
-/// <summary>Returns user inputs only if they are valid integers</summary>
+/// <summary>Returns user inputs which are valid integers</summary>
 static int GetValidInteger (char* prompt) {
    bool output;
-   int num;
+   int validInt;
    do {
       char numberStr[4096];
       printf ("%s", prompt);
@@ -45,12 +45,11 @@ static int GetValidInteger (char* prompt) {
       if (output = strchr (numberStr, '\n') != NULL) {
          errno = 0;
          char* endptr = NULL;
-         num = strtol (numberStr, &endptr, 10);
-         output = endptr != numberStr && *endptr == '\n' && errno != ERANGE;
+         validInt = strtol (numberStr, &endptr, 10);
+         output = endptr != numberStr && *endptr == '\n' && errno != ERANGE && validInt > 0;
       }
-      if (!output || num < 0) printf ("Invalid Input\n");
-   } while (!output || num < 0);
-   return num;
+   } while (!output);
+   return validInt;
 }
 
 int main () {
