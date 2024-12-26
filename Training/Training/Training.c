88a08e8bfdf44dc7de27b23cd2344998ef05a304
Sudarson <sudarson.selvakumar@trumpf.com>
2024-12-26 12:11:03 +0530

    A6: Output modified

--- Training/Training/Training.c
+++ Training/Training/Training.c
@@ -15,7 +15,7 @@
 #define ANSI_COLOR_GREEN   "\x1b[32m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
-static void PrintWrite (const wchar_t* const character, FILE* fp) {
+static void PrintWrite (wchar_t* character, FILE* fp) {
    wprintf (L"%s", character);
    fputws (character, fp);
 }
@@ -26,7 +26,7 @@ static void TestCases (char* outputFile, char* referenceFile) {
    else if (rfp == NULL) wprintf (L"Could not open reference file\n");
    else {
       wint_t outChar = fgetwc (ofp), refChar = fgetwc (rfp);
-      wprintf (L"\n Testcase: ");
+      wprintf (L"\nTestcase: ");
       while (outChar != WEOF || refChar != WEOF)
          if (outChar != refChar) {
             wprintf (ANSI_COLOR_RED L"FAILED\n" ANSI_COLOR_RESET);
@@ -43,40 +43,36 @@ static void TestCases (char* outputFile, char* referenceFile) {
 
 int main () {
    int a = _setmode (_fileno (stdout), _O_U16TEXT);
-   char* outputFile = "unicode_output.txt", * referenceFile = "reference.txt";
+   char* outputFile = "C:\\etc\\unicode_output.txt", * referenceFile = "reference.txt";
    FILE* fp = fopen (outputFile, "w, ccs=UTF-8");
    if (fp == NULL) wprintf (L"Could not open output file for writing\n");
    else {
-      const wchar_t* const pieces[2][8] = { L"♜", L"♞", L"♝", L"♛", L"♚", L"♝", L"♞", L"♜",
+      wchar_t* pieces[2][8] = { L"♜", L"♞", L"♝", L"♛", L"♚", L"♝", L"♞", L"♜",
          L"♖", L"♘", L"♗", L"♕", L"♔", L"♗", L"♘", L"♖" };
       PrintWrite (L"┏━━━━┳━━━━┳━━━━┳━━━━┳━━━━┳━━━━┳━━━━┳━━━━┓\n", fp);
       for (int i = 1; i <= 8; i++) {
-         switch (i) {
-            case 1: for (int j = 0; j < 8; j++) {
-               wprintf (L"┃  %s ", pieces[0][j]);
-               fputws (L"┃  ", fp);
-               fputws (pieces[0][j], fp);
-               fputws (L" ", fp);
-            }break;
-            case 2: for (int j = 0; j < 8; j++) PrintWrite (L"┃  ♟", fp);
-               break;
-            case 7:for (int j = 0; j < 8; j++) PrintWrite (L"┃  ♙ ", fp);
-               break;
-            case 8:for (int j = 0; j < 8; j++) {
-               wprintf (L"┃  %s ", pieces[1][j]);
-               fputws (L"┃  ", fp);
-               fputws (pieces[1][j], fp);
-               fputws (L" ", fp);
-            }break;
-            default:for (int j = 1; j <= 8; j++) PrintWrite (L"┃    ", fp);
-         }
-         PrintWrite (L"┃\n", fp);
-         if (i != 8)
-            PrintWrite (L"┣━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━┫\n", fp);
-      }
-      PrintWrite (L"┗━━━━┻━━━━┻━━━━┻━━━━┻━━━━┻━━━━┻━━━━┻━━━━┛", fp);
-      fclose (fp);
+         if (i == 1 || i == 8) {
+            int k = i == 1 ? 0 : 1;
+            for (int j = 0; j < 8; j++) {
+               wprintf (L"┃ %s  ", pieces[k][j]);
+               fputws (L"┃ ", fp);
+               fputws (pieces[k][j], fp);
+               fputws (L"  ", fp);
+            }
+         } else if (i == 2 || i == 7) {
+            wchar_t* piece = i == 2 ? L"┃ ♟  " : L"┃ ♙  ";
+            for (int j = 0; j < 8; j++)
+               PrintWrite (piece, fp);
+         } else
+            for (int j = 1; j <= 8; j++)
+               PrintWrite (L"┃    ", fp);
+      PrintWrite (L"┃\n", fp);
+      if (i != 8)
+         PrintWrite (L"┣━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━┫\n", fp);
    }
-   TestCases (outputFile, referenceFile);
-   return 0;
+   PrintWrite (L"┗━━━━┻━━━━┻━━━━┻━━━━┻━━━━┻━━━━┻━━━━┻━━━━┛", fp);
+   fclose (fp);
+}
+TestCases (outputFile, referenceFile);
+return 0;
 }
