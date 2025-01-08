a4395724c472ce1bd0926863b4df2e621c178d2e
Sudarson <sudarson.selvakumar@trumpf.com>
2025-01-08 09:25:30 +0530

    B17: Initial Commit

--- Training/Training/Training.c
+++ Training/Training/Training.c
@@ -2,13 +2,39 @@
 // Training ~ A training program for new joiners at Metamation, Batch - July 2024.
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
-// Program.c
-// Program on main branch.
+// Training.c
+// Program to print the factorial for a given number.
 // ------------------------------------------------------------------------------------------------
 
 #include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<errno.h>
+#include<string.h>
+
+static int GetValidInteger (char* prompt) {
+   bool output;
+   int validInt;
+   do {
+      char numberStr[4096];
+      printf ("%s", prompt);
+      fgets (numberStr, sizeof (numberStr), stdin);
+      if (output = strchr (numberStr, '\n') != NULL) {
+         errno = 0;
+         char* endptr = NULL;
+         validInt = strtol (numberStr, &endptr, 10);
+         output = endptr != numberStr && *endptr == '\n' && errno != ERANGE && validInt >= 0 && validInt <= 20;
+      }
+   } while (!output);
+   return validInt;
+}
+
+long long int Factorial (int num) {
+   long long int fact = num;
+   return num == 0 ? 1 : (fact *= Factorial (num - 1));
+}
 
 int main () {
-	printf ("Hello World!");
-	return 0;
+   printf ("%lld", Factorial (GetValidInteger ("Enter a number (0<=N<=20): ")));
+   return 0;
 }
