21cbf38ae0364c06b0dc4432e6574457d360bab7
Sudarson <sudarson.selvakumar@trumpf.com>
2025-01-08 10:56:15 +0530

    B19: Initial commit

--- Training/Training/Training.c
+++ Training/Training/Training.c
@@ -2,13 +2,43 @@
 // Training ~ A training program for new joiners at Metamation, Batch - July 2024.
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
-// Program.c
-// Program on main branch.
+// Training.c
+// Program to swap 2 numbers.
 // ------------------------------------------------------------------------------------------------
 
 #include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<errno.h>
+#include<string.h>
+
+static int GetValidInteger (char* prompt) {
+   bool output;
+   int validInt;
+   do {
+      char numberStr[4096];
+      printf ("%s", prompt);
+      fgets (numberStr, sizeof (numberStr), stdin);
+      if (output = strchr (numberStr, '\n') != NULL) {
+         errno = 0;
+         char* endptr = NULL;
+         validInt = strtol (numberStr, &endptr, 10);
+         output = endptr != numberStr && *endptr == '\n' && errno != ERANGE;
+      }
+   } while (!output);
+   return validInt;
+}
+
+void Swap (int* num1, int* num2) {
+   *num1 = *num1 + *num2;
+   *num2 = *num1 - *num2;
+   *num1 = *num1 - *num2;
+}
 
 int main () {
-	printf ("Hello World!");
-	return 0;
+   int num1 = GetValidInteger ("Enter a number (A): "),
+      num2 = GetValidInteger ("Enter a number (B): ");
+   if (num1 != num2) Swap (&num1, &num2);
+   printf ("A: %d\nB: %d", num1, num2);
+   return 0;
 }
